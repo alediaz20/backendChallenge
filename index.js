d666d689f156f98dc06f0e3cdcda1db023ca2a9c
Rebrit Users <rbtusers@rebrit.ar>
2024-01-03 15:42:47 -0300

    avances en routes

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const app = fastify();
 
-
 // Usuarios
 // Create
 app.post("/usuarios", async (req, res) => {
     const { nombre } = req.body;
     const usuario = await prisma.usuarios.create({
         data: {
-            nombre   //Falta asignar tareas
-        }
-    })
+        nombre, //Falta asignar tareas
+        },
+    });
     res.send(usuario);
-})
+});
 
 // Read
 app.get("/usuarios", async (req, res) => {
@@ -26,23 +25,37 @@ app.get("/usuarios", async (req, res) => {
 app.get("/usuarios/:id", async (req, res) => {
     const { id } = req.params;
     const usuario = await prisma.usuarios.findUnique({
-        where: { id: Number(id) }
+        where: { id: Number(id) },
     });
     res.send(usuario);
 });
 
 //Update
-app.put('/usuarios/:id', async(req, res) => {
+app.put("/usuarios/:id", async (req, res) => {
     const { id } = req.params;
     const { nombre } = req.body;
-    const usuario = usuarios.update({ 
-        where: { id: id },
-        data: { 
-            nombre,
+    const usuario = usuarios.update({
+        where: { id: Number(id) },
+        data: {
+        nombre,
         },
     });
     res.send(usuario);
-})
+});
+
+//Delete
+app.delete("/usuarios/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const usuario = await prisma.usuarios.delete({
+        where: { id: Number(id) },
+        });
+        res.send("El usuario " + usuario.nombre + " ha sido eliminado");
+    } catch (e) {
+        res.send("No se encontro el usuario");
+    }
+});
+
 
 
 
@@ -52,12 +65,12 @@ app.post("/tareas", async (req, res) => {
     const { texto, descripcion } = req.body;
     const tarea = await prisma.tareas.create({
         data: {
-            texto,
-            descripcion
-        }
-    })
+        texto,
+        descripcion,
+        },
+    });
     res.send(tarea);
-})
+});
 
 //Read
 app.get("/tareas", async (req, res) => {
@@ -68,7 +81,7 @@ app.get("/tareas", async (req, res) => {
 app.get("/tareas/:id", async (req, res) => {
     const { id } = req.params;
     const tareas = await prisma.tareas.findUnique({
-        where: { id: Number(id) }
+        where: { id: Number(id) },
     });
     res.send(tareas);
 });
@@ -80,26 +93,20 @@ app.get("/tareas/:id", async (req, res) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Summary
+app.get("/summary", async (req,res) => {
+    const summary = await prisma.tareas.groupBy({
+        by: ['estado'],
+    })
+    res.send(summary);
+})
 
 
 
 
 try {
     app.listen({ port: 3000 });
-    console.log("Listen on port 3000")
+    console.log("Listen on port 3000");
 } catch (error) {
     console.log(error);
 }
